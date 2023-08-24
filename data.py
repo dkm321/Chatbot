a150c48f57e2101961a25694bc81831d0a24f33a
DK Marsh <donald.marsh@teradata.com>
2023-08-24 12:33:09 -0500

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+urls = [
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Creating-a-C-or-C-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Creating-a-C-or-C-UDF/Example-Creating-a-C-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs/Call-a-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs/List-C/C-UDFs',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs/Get-Details-About-a-C/C-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs/Replace-a-C/C-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Using-UDFs/Drop-a-C/C-UDF',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Migrating-Existing-UDF-SQL-Scripts-to-VantageCloud-Lake',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Migrating-Existing-UDF-SQL-Scripts-to-VantageCloud-Lake/Running-the-External-Routine-Migration-Tool',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/Migrating-Existing-UDF-SQL-Scripts-to-VantageCloud-Lake/Example-Migrating-Existing-UDF-SQL-Scripts-to-VantageCloud-Lake',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Installation-Tool',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Installation-Tool/tdextroutine-UDF-Subcommands-and-Options',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Installation-Tool/Installing-the-External-Routine-Installation-Tool',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/UDF-Manifest-File',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/UDF-Manifest-File/Example-UDF-Manifest-File',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Migration-Tool',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Migration-Tool/Installing-the-External-Routine-Migration-Tool',
+    'https://docs.teradata.com/r/Teradata-VantageCloud-Lake/Creating-Using-and-Migrating-UDFs-to-VantageCloud-Lake/External-Routine-Installation-and-Migration-Tools/External-Routine-Migration-Tool/UDF-Config-File'
+]
+
+
+    
